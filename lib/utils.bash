a98c0c401638f5b6fc5c3c6320f60b8c720366db
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-07-15 18:00:49 -0400

    Add ammonite itself to .tool-versions

--- lib/utils.bash
+++ lib/utils.bash
@@ -19,6 +19,7 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: Bearer $GITHUB_API_TOKEN")
 fi
 
+# todo sort by only Ammonite version, not Scala version
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
